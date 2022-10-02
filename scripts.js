9759fb66ba2a1d74e6608fd779ee3ae6c0bb28b6
Brittni <brittni.toporek@gmail.com>
2022-10-01 18:10:54 -0700

    added modified html and scripts.js files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,45 @@
-ECHO is on.
+//create a variable to hold the quantity of available plane seats left on a flight
+let availablePlaneSeats = 12;
+		
+//create a variable to hold the cost of groceries at checkout
+let costOfGroceries = 51.35;
+
+//create a variable to hold a person's middle initial
+let middleInitial = 'D';
+
+//create a variable to hold true if it's hot outside and false if it's cold outside
+let isHotOutside = true;
+
+//create a variable to hold a customer's first name
+let customerFirstName = 'Mike';
+
+//create a variable to hold a street address
+let streetAddress = '12345 East This Way Lane';
+
+//print all variables to the console
+console.log(availablePlaneSeats);
+console.log(costOfGroceries);
+console.log(middleInitial);
+console.log (isHotOutside);
+console.log(customerFirstName):
+console.log(streetAddress);
+
+
+
+//a customer booked 2 plane seats, remove 2 seats from the available seats variable
+
+
+//impulse candy bar purchase, add 2.15 to the grocery total
+//costOfGroceries = costOfGroceries + 2.15;
+
+
+//birth certificate was printed incorrectly, change the middle initial to something else
+
+
+//the season has changed, update the hot outside variable to be opposite of what it was
+
+
+//create a new variable called full name using the customer's first name, the middle initial, and a last name of your choice
+
+
+//print a line to the console that introduces the customer and says they live at the address variable
